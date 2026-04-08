8b9b3e6819f387f9ca76f86081145e1ba04e2b31
Benny Zhang <luandayouzhang@ucsb.edu>
2026-04-07 23:24:36 -0700

    xy - updated with actual developer name

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -16,11 +16,7 @@ public class Developer {
      */
 
     public static String getName() {
-        // TODO: Change this to your name
-        // You may use just the name that is used on <https://bit.ly/cs156-s26-teams>
-        // i.e. your first name, or your first and initial of last name
-
-        return "Chris G.";
+        return "Benny.";
     }
 
     /**
@@ -30,7 +26,7 @@ public class Developer {
 
     public static String getGithubId() {
         // TODO: Change this to your github id
-        return "cgaucho";
+        return "BennyZll";
     }
 
     /**
@@ -40,13 +36,13 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("s26-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s26-14");
+        team.addMember("Abhijeet");
+        team.addMember("Alessandro");
+        team.addMember("Benny.");
+        team.addMember("Brian R");
+        team.addMember("Nathan C");
+        team.addMember("Vishva");
         return team;
     }
 }
